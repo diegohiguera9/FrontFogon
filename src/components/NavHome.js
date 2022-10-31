7144c0696c087ffad0be515aeaa2cc027c9785b8
Diego <drhiguerag@unal.edu.co>
2022-10-31 11:44:37 -0500

    logo

--- src/components/NavHome.js
+++ src/components/NavHome.js
@@ -12,7 +12,6 @@ const NavHome = () => {
   const [opneRegister, setOpenRegister] = useState(false);
   const [openLogin, setOpenLogin] = useState(false);
   const [openPop, setOpenPop] = useState(false);
-  const logo = process.env.PUBLIC_URL + "./Logos/person.svg";
 
   return (
     <div className="navhome">
@@ -59,13 +58,22 @@ const NavHome = () => {
       <Popover opened={openPop} onChange={setOpenPop}>
         <Popover.Target>
           <div className="navhome__img" onClick={() => setOpenPop((o) => !o)}>
-            <img src={logo} alt='logo'></img>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-person-lines-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z" />
+            </svg>
           </div>
         </Popover.Target>
 
         <Popover.Dropdown>
-          <Auth0Button/>
-          <Aut0Logout/>
+          <Auth0Button />
+          <Aut0Logout />
         </Popover.Dropdown>
       </Popover>
     </div>
